b5d8f2b39e2fd53a709501d97375408b2b2fb6a9
prishii17 <prishujardosh17@gmail.com>
2026-06-09 11:43:03 +0530

    updates

---
+++ D7/Pythondemo/ifdemo.py
@@ -0,0 +1,11 @@
+a = int(input("Bro Enter Positive Number:"))
+if a >0:
+    if a == 30:
+        print("A is 30")
+    elif a<30:
+        print("A is less than 30")
+    else:
+        print("A is greater 30")
+else:
+    print("Enter positive number only man!")
+
---
+++ D7/Pythondemo/sum.py
@@ -0,0 +1,6 @@
+a = int(input("Enter number1:"))
+b = int(input("Enter number2:"))
+c = a + b
+print("Value of A is",a)
+print("Value of B is",b)
+print("Sum is:",c)
