f14bff95dbe659b0cbd4ed7ea640550ffe678ee6
prishii17 <prishujardosh17@gmail.com>
2026-06-12 09:09:27 +0530

    updates

---
+++ D8/dynamiclist.py
@@ -0,0 +1,7 @@
+mylist = []
+count = int(input("How many records?"))
+
+for i in range(0, count ):
+    val = input("Enter your record:")
+    mylist.append(val)
+print(mylist)
---
+++ D8/mydic.py
@@ -0,0 +1,4 @@
+mydic = {"B":"Bangtan",50:"Prishi","P":"Python",99:99.90}
+
+for i in mydic:
+    print(f"Key value is {mydic[i]}")
